b04829b2f375e733d50525f5e8e514a0b50c3dd3
Avery By <averyby@yahoo.com>
2019-03-18 09:33:09 +0800

    First commit

--- index/index.js
+++ index/index.js
@@ -1,11 +1,60 @@
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
+    focused: false,
+    dialogClass: '',
+    answers: [],
+    inputValue: '',
+    scrollTop: 0
+  },
+  onLoad() {
+    // const { answers } = this.data;
+    // for(let i = 0; i < 40; i++) {
+    //   answers.push(i);
+    // }
+    // this.setData({
+    //   answers
+    // });
+  },
+  onInputFocus(e) {
+    this.setData({
+      dialogClass: 'moveToBottom',
+      focused: true,
+      scrollTop: 10000
+    });
+  },
+  onInputBlur(e) {
+    this.setData({
+      dialogClass: 'moveToTop',
+      focused: false
+    });
+  },
+  getJustifiedValue(v) {
+    // Apply validation here
+    // Returns desired value with possible modification to the raw value.
+    return v;
+  },
+  onInput({ detail }) {
+    const { value } = detail;
+    console.log('value', value);
 
+    const justifiedValue = this.getJustifiedValue(value);
+    // Keep track of the input value in data.inputValue
+    this.setData({
+      inputValue: justifiedValue
+    });
+    // The returned value has the final say on what appears in the input.
+    return justifiedValue;
   },
-  onLoad: function () {
-    console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
-    console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
+  onConfirm() {
+    const { answers, inputValue } = this.data;
+    this.setData({
+      answers: [...answers, inputValue],
+      inputValue: '' // Last but not least, empty the input box.
+    });
   },
-})
+  formReset() {
+    console.log('reset');
+  }
+});
